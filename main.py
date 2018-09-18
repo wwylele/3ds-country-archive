85360169f56b1a77d5674782ba515de803390d4d
Weiyi Wang <wwylele@gmail.com>
2018-09-18 12:28:14 -0400

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+import country
+import region
+import json
+import os
+import shutil
+from lz import decompressFile
+
+def loadFromArchive(path):
+    result = {}
+
+    regions = result["regions"] = []
+    for regionCode in ["CN", "EU", "JP", "KR", "TW", "US"]:
+        regions.append(region.loadRegionFromArchive(path, regionCode))
+
+    countries = result["countries"] = []
+
+    for i in range(256):
+        countryResult = country.loadCountryFromArchive(path, i)
+        if countryResult is not None:
+            countries.append(countryResult)
+
+    return result
+
+def writeToArchive(path, archive):
+    shutil.rmtree(path, ignore_errors = True)
+    os.mkdir(path)
+    for regionCode in ["CN", "EU", "JP", "KR", "TW", "US"]:
+        os.mkdir(os.path.join(path, regionCode))
+
+    for x in archive["regions"]:
+        region.writeRegionToArchive(path, x)
+
+
+    for x in archive["countries"]:
+        country.writeCountryToArchive(path, x)
+
+
+inPath = "/home/wwylele/3ds-country-archive/origin"
+outPath = "/home/wwylele/3ds-country-archive/new"
+archive = loadFromArchive(inPath)
+print(json.dumps(archive, indent = 2))
+writeToArchive(outPath, archive)
+
+for path, dirs, files in os.walk(inPath):
+    for f in files:
+        a = os.path.join(path, f)
+        b = os.path.join(outPath, os.path.relpath(path, inPath) ,f)
+        with open(a, "rb") as inFile, open(b, "rb") as outFile:
+            inData = decompressFile(inFile)
+            outData = outFile.read()
+            assert inData == outData
---
+++ parse_name.py
@@ -0,0 +1,30 @@
+# Python 3
+import itertools
+
+def parseName(file):
+    r = []
+    for i in range(16):
+        r.append(file.read(0x80).decode(encoding="utf_16_le").rstrip('\0'))
+    assert r[12] == r[1]
+    assert r[13] == r[1]
+    assert r[14] == r[1]
+    assert r[15] == r[1]
+    return r[0:12]
+
+def writeName(file, nameList):
+    for i in itertools.chain(range(12), [1, 1, 1, 1]):
+        encoded = nameList[i].encode(encoding="utf_16_le").ljust(0x80, b'\0')
+        file.write(encoded)
+
+def parseSort(file):
+    data = file.read(16)
+    r = [int(x) for x in data]
+    assert r[12] == r[0]
+    assert r[13] == r[0]
+    assert r[14] == r[0]
+    assert r[15] == r[0]
+    return r[0:12]
+
+def writeSort(file, sort):
+    for i in itertools.chain(range(12), [0, 0, 0, 0]):
+        file.write(sort[i].to_bytes(1, byteorder='little'))
