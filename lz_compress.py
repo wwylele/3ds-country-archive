34e6bbff2a94689245de8b979b4e3a6a9d238005
Weiyi Wang <wwylele@gmail.com>
2018-09-18 22:50:16 -0400

    remove unused import

--- lz_compress.py
+++ lz_compress.py
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
 
-from sys import stderr
-
 from collections import defaultdict
 from operator import itemgetter
 from struct import pack, unpack
